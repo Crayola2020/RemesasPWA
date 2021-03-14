41eeee8186469fcec8b3a20dd71744e5f0585c8b
seruiz <sergio.ruiz@fogade.gob.ve>
2021-03-14 05:49:04 -0400

    gh-pages commit

---
+++ precache-manifest.c8219917fc75567c0f1f5273ff8ac77d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4a6d518b48922b8e0f",
+    "url": "/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "375e6b435bea3b8cd324f3f326808e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f838e088edb4d7535ac5",
+    "url": "/js/about.504d7bb6.js"
+  },
+  {
+    "revision": "3b4a6d518b48922b8e0f",
+    "url": "/js/app.70588779.js"
+  },
+  {
+    "revision": "f077cadcb110dc8da505",
+    "url": "/js/chunk-vendors.fc010b69.js"
+  },
+  {
+    "revision": "3e5efcbe23c22c286a82abc02d849516",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
